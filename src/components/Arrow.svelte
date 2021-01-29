3a91617a28b4896cd5d14e0b86730c9c4a1cb6c1
Simo Ngquseka <simo@swssr.co>
2021-01-29 13:01:38 +0200

    Game logic crispy

---
+++ src/components/Arrow.svelte
@@ -0,0 +1,69 @@
+<div class="arrow">
+  <svg
+    width="62"
+    height="57"
+    viewBox="0 0 62 57"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g filter="url(#filter0_d)">
+      <path
+        d="M31 49L4.15321 0.250003L57.8468 0.250007L31 49Z"
+        fill="#F1AF29"
+      />
+      <path
+        d="M54.4622 2.25001L31 44.854L7.53784 2.25L54.4622 2.25001Z"
+        stroke="#033280"
+        stroke-width="4"
+      />
+    </g>
+    <defs>
+      <filter
+        id="filter0_d"
+        x="0.153214"
+        y="0.25"
+        width="61.6936"
+        height="56.75"
+        filterUnits="userSpaceOnUse"
+        color-interpolation-filters="sRGB"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="2" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow"
+          result="shape"
+        />
+      </filter>
+    </defs>
+  </svg>
+</div>
+
+<style>
+  .arrow {
+    content: "";
+    position: absolute;
+    left: 50%;
+    top: 64%;
+    transform: translate(-50%, -50%);
+    margin-top: -205px;
+    z-index: 3;
+    transform-origin: center;
+  }
+</style>
--- src/components/Fortune.svelte
+++ src/components/Fortune.svelte
@@ -1,6 +1,7 @@
 <script>
   import { onMount } from "svelte";
   import { each } from "svelte/internal";
+  import Arrow from "./Arrow.svelte";
 
   import Segment from "./Segment.svelte";
 
@@ -15,75 +16,78 @@
   let slowingDown = false;
   let pressed = {};
 
+  let optionHighestOds;
+  let highestOddsYet = 0;
+
   let gameWon;
 
-  window.addEventListener("keydown", handleKeyDown, false);
-  window.addEventListener("keyup", handleKeyUp, false);
-
-  //40deg increments from first prize
+  //45deg increments from first prize
+  //Basically brute forced the rotation
+  const increments = 43; // Will be usefull later on.
   const options = [
-    { id: "trap_1", bg: "", rotation: { from: 3498, to: 3539 } },
-    { id: "savanna_pack_bottle", bg: "", rotation: { from: 3539, to: 3585 } },
-    { id: "4th_yellow", bg: "", rotation: { from: 3585, to: 3630 } },
-    { id: "trap_2", bg: "", rotation: { from: 3630, to: 3675 } },
-    { id: "paarl", bg: "", rotation: { from: 3675, to: 3720 } },
-    { id: "hunters_pack", bg: "", rotation: { from: 3720, to: 3765 } },
-    { id: "trap_3", bg: "", rotation: { from: 3765, to: 3810 } },
+    { id: "trap_1", odds: 90, bg: "", rotation: { from: 3498, to: 3539 } },
+    {
+      id: "savanna_pack_bottle",
+      odds: 60,
+      bg: "",
+      rotation: { from: 3539, to: 3585 },
+    },
+    { id: "4th_yellow", odds: 10, bg: "", rotation: { from: 3585, to: 3630 } },
+    { id: "trap_2", odds: 0, bg: "", rotation: { from: 3630, to: 3675 } },
+    { id: "paarl", odds: 10, bg: "", rotation: { from: 3675, to: 3720 } },
+    {
+      id: "hunters_pack",
+      odds: 100,
+      bg: "",
+      rotation: { from: 3720, to: 3765 },
+    },
+    { id: "trap_3", odds: 0, bg: "", rotation: { from: 3765, to: 3810 } },
   ];
 
   const _traps = options.filter((v) => v.id.includes("trap"));
   const _wins = options.filter((v) => !v.id.includes("trap"));
 
   console.log(_traps);
-  const odds = 0.7;
-  const _randomNum = Math.random();
+  const gameOdds = 100;
+  const _randomNum = Math.random() * 100;
 
-  console.log("odds", odds);
+  console.log("odds", gameOdds);
   console.log("random number", _randomNum);
 
-  gameWon = _randomNum > odds;
+  gameWon = _randomNum < gameOdds;
+
+  console.log("Option with the highest odds:", { optionHighestOds });
 
   if (gameWon) {
     //TODO: Need to randomly select from _wins range.
     rotation = 3730;
     console.log("Should win");
+
+    //Compute prize with highest winning odds.
+    console.log({ _wins });
+    for (const _option of _wins) {
+      if (_option.odds >= highestOddsYet) {
+        optionHighestOds = _option;
+        highestOddsYet = _option.odds;
+
+        console.log("Should win", { optionHighestOds });
+        rotation =
+          optionHighestOds.rotation.from +
+          Math.floor(Math.random() * increments);
+      }
+    }
   } else {
     //TODO: Need to randomly select from _trap range.
     console.log("Should lose");
-    rotation = 3640;
-  }
-
-  function handleKeyDown(e) {
-    if (e.keyCode === 32 && slowingDown === false) {
-      animateKeyOut();
-      if (pressed[e.which]) return;
-      pressed[e.which] = e.timeStamp;
-    }
-  }
-
-  function handleKeyUp(e) {
-    if (e.keyCode === 32) {
-      animateKeyIn();
-      spinThatWheel(e);
-      console.log({ e });
-    }
-  }
-
-  function animateKeyOut() {
-    scale -= 0.03;
-    if (scale < 0.5) return;
-    TweenMax.to(resetBtn, 0.5, { ease: Sine.easeOut, scale: scale });
-  }
-
-  function animateKeyIn() {
-    scale = 1;
-    TweenMax.to(resetBtn, 0.25, { scale: scale });
+    console.log(_traps);
+    rotation =
+      _traps[Math.floor(Math.random() * _traps.length - 1)].rotation.from +
+      Math.floor(Math.random() * increments);
   }
 
   function spinThatWheel(e) {
     animateWheel(spinDuration);
     slowingDown = true;
-    pressed[e.which] = 0;
   }
 
   function animateWheel(duration) {
@@ -98,7 +102,11 @@
     console.log({ gameWon });
     slowingDown = false;
     rotation = 0;
-    alert(gameWon ? "Awesome! You won." : "Sorry, you lost.");
+    alert(
+      gameWon
+        ? `Awesome! You won. a ${optionHighestOds.id}`
+        : "Sorry, you lost."
+    );
   }
 
   onMount(() => {
@@ -112,71 +120,16 @@
     <h1>WHEEL OF FORTUNE</h1>
     <p>TAP <strong>SPIN WHEEL</strong> TO PLAY</p>
   </header>
-
-  <div class="arrow">
-    <svg
-      width="62"
-      height="57"
-      viewBox="0 0 62 57"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g filter="url(#filter0_d)">
-        <path
-          d="M31 49L4.15321 0.250003L57.8468 0.250007L31 49Z"
-          fill="#F1AF29"
-        />
-        <path
-          d="M54.4622 2.25001L31 44.854L7.53784 2.25L54.4622 2.25001Z"
-          stroke="#033280"
-          stroke-width="4"
-        />
-      </g>
-      <defs>
-        <filter
-          id="filter0_d"
-          x="0.153214"
-          y="0.25"
-          width="61.6936"
-          height="56.75"
-          filterUnits="userSpaceOnUse"
-          color-interpolation-filters="sRGB"
-        >
-          <feFlood flood-opacity="0" result="BackgroundImageFix" />
-          <feColorMatrix
-            in="SourceAlpha"
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-          />
-          <feOffset dy="4" />
-          <feGaussianBlur stdDeviation="2" />
-          <feColorMatrix
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-          />
-          <feBlend
-            mode="normal"
-            in2="BackgroundImageFix"
-            result="effect1_dropShadow"
-          />
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="effect1_dropShadow"
-            result="shape"
-          />
-        </filter>
-      </defs>
-    </svg>
-  </div>
-
+  <Arrow />
   <div bind:this={wheel} class="wheel">
     {#each options as { id, bg } (id)}
       <Segment value={bg} ref={bg} />
     {/each}
   </div>
 
-  <button bind:this={resetBtn} class="btn">SPIN WHEEL</button>
+  <button on:click={spinThatWheel} bind:this={resetBtn} class="btn"
+    >SPIN WHEEL</button
+  >
 </div>
 
 <style>
@@ -187,22 +140,11 @@
   .logo {
     width: 100%;
     height: min-content;
-    margin-top: 2em;
     color: #fff;
   }
   h1 {
     font-size: 28px;
     color: var(--color-gold);
-  }
-  .arrow {
-    content: "";
-    position: absolute;
-    left: 50%;
-    top: 64%;
-    transform: translate(-50%, -50%);
-    margin-top: -205px;
-    z-index: 3;
-    transform-origin: center;
   }
 
   .wheel {
@@ -218,8 +160,6 @@
     top: 55%;
     left: 50%;
     overflow: hidden;
-
-    /* box-shadow: 0px 0px 10px 10px #000; */
     transform: scale(0.75);
   }
 
@@ -235,16 +175,5 @@
     text-transform: uppercase;
     background: #fff;
     width: 80%;
-  }
-  .reset-btn:focus {
-    outline: none;
-  }
-  .reset-btn:disabled {
-    box-shadow: none;
-    opacity: 0.5;
-    color: grey;
-  }
-  .reset-btn:active {
-    box-shadow: none;
   }
 </style>
